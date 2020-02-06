d04acfe86081479f2fe7ac3d4a790ea22620d838
jantocv <joseantoniocamina@spotahome.com>
2020-02-06 19:14:05 +0100

    More test cases

--- t1/__tests__/palindrome.test.js
+++ t1/__tests__/palindrome.test.js
@@ -1,8 +1,13 @@
 const { checkIfPalindrom } = require('../checkIfPalindrom');
 
 describe('palindrom unit test', () => {
-  it('Returns true if given string is palindrom', () => {
+  it('Returns true if given string is palindrom with odd number of characters', () => {
     const input = 'RADAR';
+    expect(checkIfPalindrom(input)).toBe(true);
+  });
+  
+  it('Returns true if given string is palindrom with a even number of characters', () => {
+    const input = 'RADDAR';
     expect(checkIfPalindrom(input)).toBe(true);
   });
 
@@ -11,8 +16,13 @@ describe('palindrom unit test', () => {
     expect(checkIfPalindrom(input)).toBe(false);
   });
 
-  it('Returns true if given strings is a palindrom with accents, diacritics and symbols', () => {
+  it('Returns true if given strings is a palindrom with accents, diacritics and symbols - odd number of characters', () => {
     const input = 'RäD-.-dÁr';
+    expect(checkIfPalindrom(input)).toBe(true);
+  });
+
+  it('Returns true if given strings is a palindrom with accents, diacritics and symbols - even number of characters', () => {
+    const input = 'RäD--dÁr';
     expect(checkIfPalindrom(input)).toBe(true);
   });
 
